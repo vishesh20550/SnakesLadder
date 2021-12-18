b0240efcf0c0558167cf8b32ce3a49aaa8d4d0e9
Vishesh Jain <vishesh20550@iiitd.ac.in>
2021-12-18 09:50:49 +0530

    Start

--- src/main/java/com/example/snakesladder/IntroController.java
+++ src/main/java/com/example/snakesladder/IntroController.java
@@ -1,14 +1,15 @@
 package com.example.snakesladder;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class IntroController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
--- src/main/java/com/example/snakesladder/MainApplication.java
+++ src/main/java/com/example/snakesladder/MainApplication.java
@@ -11,8 +11,9 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("IntroPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(),400,550);
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
 
